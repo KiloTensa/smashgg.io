1e891bf9d29b229cd53efdba808254b530d1440f
KiloTensa <96514133+KiloTensa@users.noreply.github.com>
2026-05-21 14:20:30 +0000

    fix for fix preloading

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import SmashGunGame from './pages/SmashGunGame';
-import IntroLanding from '@/components/IntroLanding';
+import Intro from '@/components/Intro';
 import { useState } from 'react';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClientInstance}>
-      {showIntro && <IntroLanding onIntroComplete={() => setShowIntro(false)} />}
+      {showIntro && <Intro onFinish={() => setShowIntro(false)} />}
       <Router basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<SmashGunGame />} />
