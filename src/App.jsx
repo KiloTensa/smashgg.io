9ab128dad2ca40e7cb011804b6e9b80318075c0a
KiloTensa <96514133+KiloTensa@users.noreply.github.com>
2026-05-21 00:37:04 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClientInstance}>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<SmashGunGame />} />
           <Route path="*" element={<PageNotFound />} />
